047e632a29ce800c247664d232f703fe07f8589d
James Wickenden <jameswickenden14@gmail.com>
2021-02-22 01:24:17 +0000

    added relevant editor examples and reworked routing

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .get('/', (req, res) => {
     console.log("Loading main...")
+    res.sendFile(path.join(__dirname, 'public/old.html'))
+  })
+  .get('/tree', (req, res) => {
+    console.log("Loading tree...")
     res.sendFile(path.join(__dirname, 'public/tree.html'))
   })
   .get('/version1', (req, res) => {
@@ -15,5 +19,13 @@ express()
   .get('/version2', (req, res) => {
     console.log("Loading UI Version 2: Separate tabs...")
     res.sendFile(path.join(__dirname, 'public/version2.html'))
+  })
+  .get('/editor1', (req, res) => {
+    console.log("Loading graph editor A...")
+    res.sendFile(path.join(__dirname, 'public/editor1.html'))
+  })
+  .get('/editor2', (req, res) => {
+    console.log("Loading graph editor B...")
+    res.sendFile(path.join(__dirname, 'public/editor2.html'))
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
