15d0fd89b97d897aa07d35ec5a28b292791303b0
James Wickenden <jameswickenden14@gmail.com>
2021-02-22 01:03:24 +0000

    added handling for UI versions

--- server.js
+++ server.js
@@ -8,4 +8,12 @@ express()
     console.log("Loading main...")
     res.sendFile(path.join(__dirname, 'public/tree.html'))
   })
+  .get('/version1', (req, res) => {
+    console.log("Loading UI Version 1: Adjustable windows...")
+    res.sendFile(path.join(__dirname, 'public/version1.html'))
+  })
+  .get('/version2', (req, res) => {
+    console.log("Loading UI Version 2: Separate tabs...")
+    res.sendFile(path.join(__dirname, 'public/version2.html'))
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
