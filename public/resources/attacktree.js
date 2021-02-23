acdafa5778b518bb6027657136c1a2d6d91c08e6
James Wickenden <jameswickenden14@gmail.com>
2021-02-23 18:00:06 +0000

    started building the attack tree internally

---
+++ public/resources/attacktree.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function instantiateTree(goal) {
+    const atree = {};
+    atree.nodes = [];
+
+}
+
+function addNode() {
+    const node = {};
+    node.goal = "Something";
+    node.attributes = [];
+    node.parent = -1;
+}
