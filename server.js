61756e4814219a46c60e7501e19a4911ed810279
James Wickenden <jameswickenden14@gmail.com>
2021-03-05 18:31:51 +0000

    finished implementing socket.io chat example. testing on heroku now

--- server.js
+++ server.js
@@ -7,7 +7,17 @@ const PORT = process.env.PORT || 5000;
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-app
+setup_express();
+setup_socket_io();
+
+http.listen(PORT, () => {
+  console.log(`Listening on port ${ PORT }`);
+});
+
+
+// Sets up express and its /GET page handlers
+function setup_express() {
+  app
   .use(express.static(path.join(__dirname, 'public')))
   .get('/', (req, res) => {
     console.log("Loading main...")
@@ -28,13 +38,22 @@ app
   .get('/tree_builder', (req, res) => {
     console.log("Loading tree builder...")
     res.sendFile(path.join(__dirname, '/public/tree_builder.html'))
-  })
+  });
   //.listen(PORT, () => console.log(`Listening on port ${ PORT }`));
+};
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-});
+// Sets up the socket.io events and handlers
+function setup_socket_io() {
+  io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+    socket.on('chat message', (msg) => {
+      console.log('message: ' + msg);
+      io.emit('chat message', msg);
+    });
+  });
 
-http.listen(PORT, () => {
-  console.log(`Listening on port ${ PORT }`);
-});
+  io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
+};
