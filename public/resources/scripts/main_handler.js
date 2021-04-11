6b9131f9218b24afde7d71354f7a3f6f79da43f5
James Wickenden <jameswickenden14@gmail.com>
2021-04-11 20:51:45 +0100

    more design work on homepage

--- public/resources/scripts/main_handler.js
+++ public/resources/scripts/main_handler.js
@@ -7,12 +7,11 @@ function SetUpMainLinks() {
     var join_group = document.getElementById('main_join_group');
     
     private_editor.onclick = function(evt) { LoadPrivateEditor(evt) };
-    create_group.onclick = function(evt) { CreateGroupEditor(evt) };
-    join_group.onclick = function(evt) { JoinGroupEditor(evt) };
+    create_group.onclick = function(evt) { SetupGroupInput(evt, create_group) };
+    join_group.onclick = function(evt) { SetupGroupInput(evt, join_group) };
 };
 
-function LoadPrivateEditor(evt) {
-    evt.preventDefault();
+function LoadPrivateEditor() {
     sessionStorage.setItem('is_private', 'true');
     sessionStorage.removeItem('group_key');
     console.log(sessionStorage.getItem('is_private'));
@@ -20,15 +19,26 @@ function LoadPrivateEditor(evt) {
     window.location.href = '/tree_builder';
 };
 
-function CreateGroupEditor(evt) {
+function SetupGroupInput(evt, sender) {
     evt.preventDefault();
+    document.getElementById('main_create_group').style.backgroundColor = '#bbb';
+    document.getElementById('main_join_group').style.backgroundColor = '#bbb';
+    sender.style.backgroundColor = 'green';
+
+    var group_input = document.getElementById('right_options');
+    group_input.style.display = 'flex';
+
+};
+
+function CreateGroupEditor() {
     console.log(socket);
     socket.emit('group_key_avl_req', '1234');
+    SetupGroupInput();
 };
 
 function JoinGroupEditor(evt) {
     evt.preventDefault();
-
+    SetupGroupInput();
 };
 
 socket.on('group_key_avl', function (data) {
