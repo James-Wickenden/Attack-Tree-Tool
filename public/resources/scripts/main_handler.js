0509e5354e0ec340a4982c043ac76c9d3c3b6c42
James Wickenden <jameswickenden14@gmail.com>
2021-04-11 22:05:20 +0100

    main page group code should be done. need to update it on the builder page

--- public/resources/scripts/main_handler.js
+++ public/resources/scripts/main_handler.js
@@ -1,46 +1,120 @@
 'use safe';
-var socket = io();
+
+var socket;
 
 function SetUpMainLinks() {
+    SetUpSocket();
+    SetUpHoverHighlight();
+
     var private_editor = document.getElementById('main_private_editor');
     var create_group = document.getElementById('main_create_group');
     var join_group = document.getElementById('main_join_group');
     
-    private_editor.onclick = function(evt) { LoadPrivateEditor(evt) };
-    create_group.onclick = function(evt) { SetupGroupInput(evt, create_group) };
-    join_group.onclick = function(evt) { SetupGroupInput(evt, join_group) };
+    private_editor.onclick = function(evt) { LoadOption(evt, this, 'private') };
+    create_group.onclick = function(evt) { LoadOption(evt, this, 'create_group') };
+    join_group.onclick = function(evt) { LoadOption(evt, this, 'join_group') };
 };
 
-function LoadPrivateEditor() {
-    sessionStorage.setItem('is_private', 'true');
+// When an option is clicked, set up the session storage and reset css for highlighting.
+function LoadOption(evt, sender, mode) {
+    evt.preventDefault();
+    sessionStorage.setItem('mode', mode);
     sessionStorage.removeItem('group_key');
-    console.log(sessionStorage.getItem('is_private'));
-    console.log(sessionStorage.getItem('group_key'));
-    window.location.href = '/tree_builder';
+    OpenButton(sender);
 };
 
-function SetupGroupInput(evt, sender) {
-    evt.preventDefault();
-    document.getElementById('main_create_group').style.backgroundColor = '#bbb';
-    document.getElementById('main_join_group').style.backgroundColor = '#bbb';
+// Handles clicking on buttons and stores the active button
+function OpenButton(sender) {
+    SetUpHoverHighlight();
+
+    sender.className += ' active';
     sender.style.backgroundColor = 'green';
+    sender.onmouseleave = function(){};
 
-    var group_input = document.getElementById('right_options');
-    group_input.style.display = 'flex';
+    // Manages the start button css and handlers
+    var start_button = document.getElementById('main_group_start');
+    start_button.disabled = false;
+    start_button.style.cursor = 'pointer';
+    start_button.onmouseenter = function() { this.style.backgroundColor = 'green'; };
+    start_button.onmouseleave = function() { this.style.backgroundColor = '#bbb'; };
+    start_button.onclick = function(evt) {
+        evt.preventDefault();
+        // Private sessions can open straight away.
+        // Group sessions must first have the key verified;
+        // creating group only if that key is not in use, joining only if that group exists.
+        if (sessionStorage.getItem('mode') == 'private') {
+            OpenEditor(true, null);
+        }
+        else {
+           RequestGroupAvailablility(); 
+        }
+    };
 
+    document.getElementById('main_group_status').innerText = '';
+    var group_key_input = document.getElementById('main_group_key');
+    if (sessionStorage.getItem('mode') == 'private') {
+        group_key_input.disabled = true;
+    }
+    else {
+        group_key_input.disabled = false;
+    }
 };
 
-function CreateGroupEditor() {
-    console.log(socket);
-    socket.emit('group_key_avl_req', '1234');
-    SetupGroupInput();
+// Sends a socket message to the server asking for that group availability
+function RequestGroupAvailablility() {
+    var group_key = document.getElementById('main_group_key').value;
+    if (group_key == '') {
+        document.getElementById('main_group_status').innerText = 'Enter a group code, or start a private session.';
+        return;
+    }
+    socket.emit('group_key_avl_req', group_key);
 };
 
-function JoinGroupEditor(evt) {
-    evt.preventDefault();
-    SetupGroupInput();
+// When hovering over the main buttons, they should be green
+// For some reason, clicking buttons and mousing over the others breaks the hover css, so this is a workaround.
+function SetUpHoverHighlight() {
+    var main_buttons = document.getElementsByClassName('option_buttons');
+    for (i = 0; i < main_buttons.length; i++) {
+        main_buttons[i].style.backgroundColor = '#bbb';
+        main_buttons[i].className = main_buttons[i].className.replace(' active', '');
+        main_buttons[i].onmouseenter = function() { this.style.backgroundColor = 'green'; };
+        main_buttons[i].onmouseleave = function() { this.style.backgroundColor = '#bbb'; };
+    }
 };
 
-socket.on('group_key_avl', function (data) {
-    console.log(data);
-});
+// Sets up socket and its method for receiving messages from the server.
+function SetUpSocket() {
+    socket = io();
+    socket.on('group_key_avl', function (data) {
+        OpenEditor(false, data);
+    });
+};
+
+// Finally, open either a private session or a group session.
+function OpenEditor(private, data) {
+    if (private) {
+        window.location.href = '/tree_builder';
+    }
+    else {
+        switch (sessionStorage.getItem('mode')) {
+            case 'join_group': 
+                if (data.OK == 'OK') {
+                    document.getElementById('main_group_status').innerText = 'No such group with that code.';
+                }
+                else {
+                    sessionStorage.setItem('group_key', data.group_key);
+                    window.location.href = '/tree_builder';
+                }
+                break;
+            case 'create_group':
+                if (data.OK == 'OK') {
+                    sessionStorage.setItem('group_key', data.group_key);
+                    window.location.href = '/tree_builder';
+                }
+                else {
+                    document.getElementById('main_group_status').innerText = 'That group code is already in use.';
+                }   
+                break;
+        };
+    }
+};
